46f9114f5783f0ec662b2064d0aa59e05c93d75d
VelmaAkoch <velmaakochi1@gmail.com>
2022-05-04 18:52:33 +0300

    add getters and setters for Squad generated

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -15,4 +15,34 @@ public class Squad {
     public void setId(int id) {
         this.id = id;
     }
+    public Squad(String maxSize, String squadName, String squadCause) {
+        this.maxSize = maxSize;
+        this.squadName = squadName;
+        this.squadCause = squadCause;
+
+    }
+
+    public String getMaxSize() {
+        return maxSize;
+    }
+
+    public void setMaxSize(String maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public String getSquadName() {
+        return squadName;
+    }
+
+    public void setSquadName(String squadName) {
+        this.squadName = squadName;
+    }
+
+    public String getSquadCause() {
+        return squadCause;
+    }
+
+    public void setSquadCause(String squadCause) {
+        this.squadCause = squadCause;
+    }
 }
