67ce7ee522b3cedcf30171706a65bfb49c587aef
VelmaAkoch <velmaakochi1@gmail.com>
2022-05-04 17:36:04 +0300

    add new file of model and packages

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,8 @@
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.lang.reflect.MalformedParameterizedTypeException;
 import java.util.HashMap;
+import java.util.Map;
 
 import static spark.Spark.*;
 
@@ -27,6 +29,24 @@ public class App {
         get("hero",((request, response) -> {
             return modelAndView(new HashMap<>(), "hero.hbs");
         }), new HandlebarsTemplateEngine());
+
+        get("/squad",((request, response) -> {
+            Map<String,Object> data = new HashMap<>();
+
+            //get inputs from form
+            String name = request.queryParams("hero");
+            String age = request.queryParams("age");
+            String specialPower = request.queryParams("power");
+            String weakness = request.queryParams("weakness");
+            List<Hero>heroList = request.session().attribute("Heroes");
+
+            //create hero instance from data
+            Hero hero = new Hero(name,age,specialPower,weakness);
+
+
+
+
+        }));
 
 
 
