41eeacb6ba4fda37c4a15c55d68651e0dd8bc9f7
VelmaAkoch <velmaakochi1@gmail.com>
2022-05-04 18:22:07 +0300

    add public static list of get all heroes and set all heroes

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,9 +1,35 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hero {
     private String name;
     private String age;
     private String specialPower;
     private String weakness;
     private int heroId;
+
+    private static List<Hero>allHeroes = new ArrayList<>();
+
+    public static List<Hero> getAllHeroes() {
+        return allHeroes;
+        }
+
+        public static void  setAllHeroes(List<Hero> allHeroes) {
+        Hero.allHeroes = allHeroes;
+
+    }
+
+    public int getHeroId() {
+        return heroId;
+    }
+
+    public void setHeroId(int heroId) {
+        this.heroId = heroId;
+    }
+
+
+
+
 }
