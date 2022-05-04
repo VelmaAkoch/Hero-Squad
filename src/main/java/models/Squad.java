396e7b7556480b2393d592de7d86d43588cb9a9e
VelmaAkoch <velmaakochi1@gmail.com>
2022-05-04 18:45:00 +0300

    add getters and setters of id generated

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -1,4 +1,18 @@
 package models;
 
+import java.security.PrivateKey;
+
 public class Squad {
+    private  String maxSize;
+    private  String squadName;
+    private String squadCause;
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
