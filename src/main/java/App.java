88664ab09647b78cd6605004035083ead37dabb5
VelmaAkoch <velmaakochi1@gmail.com>
2022-05-07 17:02:34 +0300

    add all changes done on app main and background colour

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -19,84 +19,86 @@ public class App {
 
         if (process.environment().get("PORT") != null) {
             port = Integer.parseInt(process.environment().get("PORT"));
-        }
-
-        else {
+        }else {
             port = 4567;
         }
         port(port);
 
         staticFileLocation("/public/");
 
-        get("/",((request, response) ->  {
-            return modelAndView(new HashMap<>(), "index.hbs");
-        }), new HandlebarsTemplateEngine());
+        //home route
+        get("/", (request, response) -> {
+            return modelAndView( new HashMap<>(), "index.hbs");
+        }, new HandlebarsTemplateEngine());
 
-        get("hero",((request, response) -> {
-            return modelAndView(new HashMap<>(), "hero.hbs");
-        }), new HandlebarsTemplateEngine());
+        //hero route
+        get("/hero", (request, response) -> {
+            return modelAndView( new HashMap<>(), "hero.hbs");
+        }, new HandlebarsTemplateEngine());
 
-        get("/squad",((request, response) -> {
-            return modelAndView(new HashMap<>(), "squad.hbs");
-        }),new HandlebarsTemplateEngine());
+        //squad route
+        get("/squad", (request, response) -> {
+            return modelAndView( new HashMap<>(), "squad.hbs");
+        }, new HandlebarsTemplateEngine());
 
-
-        post("/hero/new", ((request, response) -> {
-            Map<String,Object> data = new HashMap<>();
-
+        //post a new hero
+        post("/hero/new", (request, response) -> {
+            Map <String,Object> data = new HashMap<>();
+            //get input from form
             String name = request.queryParams("hero");
             String age = request.queryParams("age");
             String specialPower = request.queryParams("power");
             String weakness = request.queryParams("weakness");
-            List<Hero>heroList = request.session().attribute("Heroes");
-
+            List<Hero>heroList=request.session().attribute("Heroes");
+            //create hero instance from form data
             Hero hero = new Hero(name,age,specialPower,weakness);
             HeroService heroService=new HeroService();
-
+            //Add hero to list
             heroService.addHero(heroList, hero);
             List <Hero> updatedList = heroService.getHeroList();
             request.session().attribute("Heroes", updatedList);
             System.out.println(updatedList);
-
+            //add key-value data to map for displaying to the template
             data.put("Heroes", request.session().attribute("Heroes"));
-            return new ModelAndView(data, "hero-new-hbs");
-        }), new HandlebarsTemplateEngine());
-
-        get("/hero/new", ((request, response) -> {
-            Map<String,Object> data = new HashMap<>();
-            data.put("Heroes", request.session().attribute("yourHeroList"));
             return new ModelAndView(data, "hero-new.hbs");
-        }), new HandlebarsTemplateEngine());
+        }, new HandlebarsTemplateEngine());
 
+        //get new heroes list template
+        get("/hero/new", (request, response) -> {
+            Map<String,Object>data=new HashMap<>();
+            data.put("Heroes", request.session().attribute("yourHeroList"));
+            return new ModelAndView( data, "hero-new.hbs");
+        }, new HandlebarsTemplateEngine());
 
-        post("/squad/new", ((request, response) -> {
-            Map<String,Object> data = new HashMap<>();
-
+        //post new squad
+        post("/squad/new", (request, response) -> {
+            Map <String,Object> data = new HashMap<>();
+            //get input from form
             String name = request.queryParams("squad");
             String maxSize = request.queryParams("size");
             String squadCause = request.queryParams("task");
             List <Squad> squadList = request.session().attribute("Squads");
-
+            //create squad instance from form data
             Squad squad = new Squad(maxSize, name, squadCause);
-            SquadService squadService = new SquadService();
+            SquadService squadService=new SquadService();
             squadService.addSquad(squadList, squad);
-
-            List<Squad> updatedSquadList = squadService.getSquadList();
+            //Add squad to list
+            List <Squad> updatedSquadList = squadService.getSquadList();
             request.session().attribute("Squads", updatedSquadList);
             System.out.println(updatedSquadList);
-
+            //add key-value data to map for displaying to the template
             data.put("Squads", request.session().attribute("Squads"));
             return new ModelAndView(data, "squad-new.hbs");
-        }),new HandlebarsTemplateEngine());
+        }, new HandlebarsTemplateEngine());
 
-
-        get("/squad/new",((request, response) -> {
-            Map<String,Object>data = new HashMap<>();
+        //get new squads display template
+        get("/squad/new", (request, response) -> {
+            Map<String,Object>data=new HashMap<>();
             data.put("Squads", request.session().attribute("yourSquadList"));
-            return new ModelAndView(data, "squad-new.hbs");
-        }), new HandlebarsTemplateEngine());
+            return new ModelAndView( data, "squad-new.hbs");
+        }, new HandlebarsTemplateEngine());
 
     }
-
-
 }
+
+
